95fd2c704dee13b20d63a531dc94d2f0e1567356
Dennis Meyer <dennism3991@googlemail.com>
2022-02-10 16:45:18 +0100

    Styling

---
+++ style/components/footer.stories.js
@@ -0,0 +1,7 @@
+import DefaultHtml from "./footer.default.html";
+
+export default {
+  title: "components/footer",
+};
+
+export const Default = () => DefaultHtml;
---
+++ style/components/head.stories.js
@@ -0,0 +1,7 @@
+import DefaultHtml from "./head.default.html";
+
+export default {
+  title: "components/head",
+};
+
+export const Default = () => DefaultHtml;
