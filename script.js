c737fd77e2883e0afbd4852e912128a72b2f4598
Dennis Meyer <dennism3991@googlemail.com>
2022-02-17 16:47:23 +0100

    Updated

--- script.js
+++ script.js
@@ -1,16 +1,19 @@
-
 function countChars(obj) {
-  document.getElementById("charNum").textContent = obj.value.length+' characters';
+  document.getElementById('charNum').textContent =
+    obj.value.length + ' characters';
 }
+
+countChars();
 
 const btn = document.querySelector('[data-js=answerButton1]');
 const btn2 = document.querySelector('[data-js=answerButton2]');
 const answer1 = document.querySelector('.answer1');
-const answer2 = document.querySelector('.answer2')
+const answer2 = document.querySelector('.answer2');
 
 btn.addEventListener('click', () => {
   answer1.classList.toggle('hide');
 });
+
 btn2.addEventListener('click', () => {
   answer2.classList.toggle('hide');
 });
